d4cf3bcb492fafbd494f09dfeb69177dacbe508c
Harkame <louisr.daviaud@gmail.com>
2019-04-15 13:51:47 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='japscandownloader',
+    name='animedownloader',
     version='0.0.1',
     author='Harkame',
-    description='Script to download mangas from JapScan',
+    description='Script to download animes from Jetanimes',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/Harkame/JapScanDownloader',
+    url='https://github.com/Harkame/AnimeDownloader',
     packages=find_packages(),
     classifiers=[
-        'Manga downloader',
-        'Japscan'
+        'Anime downloader',
+        'Jetanimes'
     ],
 )
